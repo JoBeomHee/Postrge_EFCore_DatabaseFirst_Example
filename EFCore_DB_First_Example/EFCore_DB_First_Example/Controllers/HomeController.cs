60de9b207427c05bef617ac75491ea81d7ad3f52
BeomHee <>
2022-11-27 22:27:16 +0900

    추가

--- EFCore_DB_First_Example/EFCore_DB_First_Example/Controllers/HomeController.cs
+++ EFCore_DB_First_Example/EFCore_DB_First_Example/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EFCore_DB_First_Example.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EFCore_DB_First_Example.Controllers
@@ -7,15 +8,17 @@ namespace EFCore_DB_First_Example.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MireroContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MireroContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Tasks.ToListAsync());
         }
 
         public IActionResult Privacy()
